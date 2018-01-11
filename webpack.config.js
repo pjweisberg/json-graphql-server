e0e42503c0fdcc66f93cc438d2842e0f5e2a5251
gildas <gildas.garcia@gmail.com>
2018-01-11 17:04:53 +0100

    Review

--- webpack.config.js
+++ webpack.config.js
@@ -1,45 +1,55 @@
 const path = require('path');
 const webpack = require('webpack');
+const DefinePlugin = webpack.DefinePlugin;
 const UglifyJsPlugin = webpack.optimize.UglifyJsPlugin;
 
 let libraryName = 'json-graphql-server';
 
-let plugins = [], outputFile;
+let plugins = [];
+let outputFile;
 
 if (process.env.NODE_ENV === 'production') {
-    plugins.push(new UglifyJsPlugin({
-        minimize: true,
-        sourceMap: true,
-    }));
-    outputFile = libraryName + '.min.js';
+    plugins.push(
+        new UglifyJsPlugin({
+            minimize: true,
+            sourceMap: true,
+        }),
+        new DefinePlugin({
+            'process.env.NODE_ENV': JSON.stringify('production'),
+        })
+    );
+    outputFile = libraryName + '.[name].min.js';
 } else {
-    outputFile = libraryName + '.js';
+    outputFile = libraryName + '.[name].js';
 }
 
 const config = {
-    entry: __dirname + '/src/index.js',
+    entry: {
+        node: __dirname + '/src/node.js',
+        client: __dirname + '/src/client.js',
+    },
     devtool: 'source-map',
     output: {
         path: __dirname + '/lib',
         filename: outputFile,
         library: libraryName,
         libraryTarget: 'umd',
-        umdNamedDefine: true
+        umdNamedDefine: true,
     },
     module: {
         rules: [
             {
                 test: /(\.jsx|\.js)$/,
                 loader: 'babel-loader',
-                exclude: /(node_modules|bower_components)/
-            }
-        ]
+                exclude: /(node_modules|bower_components)/,
+            },
+        ],
     },
     resolve: {
         modules: [path.resolve('./node_modules'), path.resolve('./src')],
-        extensions: ['.json', '.js']
+        extensions: ['.json', '.js'],
     },
-    plugins: plugins
+    plugins,
 };
 
 module.exports = config;
